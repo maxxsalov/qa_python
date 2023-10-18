5b0b7737a9f93f25b3de2e57ba68b022d6763a2a
maxim.salov <maxim.salov@aliexpress.ru>
2023-10-18 20:05:21 +0300

    add tests, add parametryze

--- tests.py
+++ tests.py
@@ -1,16 +1,29 @@
+import pytest
+
+
 class TestBooksCollector:
 
     def test_add_new_book_add_two_books(self, collector):
-        assert len(collector.get_books_genre()) == 5
+        collector.add_new_book('Шерлок Холмс')
+        collector.add_new_book('Код да Винчи')
+        assert len(collector.get_books_genre()) == 7
 
-    def test_get_book_genre_check_genre(self, collector):
-        assert collector.get_book_genre('Дюна')
+    @pytest.mark.parametrize('name', ['Гордость и предубеждение и зомби',
+                                      'Щелкунчик',
+                                      'Горе от ума',
+                                      'Пуаро',
+                                      'Дюна'])
+    def test_get_book_genre_check_genre(self, collector, name):
+        print(collector.get_book_genre(name))
+        assert collector.get_book_genre(name), f'{collector.get_book_genre(name)}'
 
     def test_get_books_genre(self, collector):
         assert collector.get_books_genre()
 
-    def test_get_books_with_specific_genre(self, collector):
-        assert collector.get_books_with_specific_genre('Детективы')
+    @pytest.mark.parametrize('genre', ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии'])
+    def test_get_books_with_specific_genre(self, collector, genre):
+        print(collector.get_books_with_specific_genre(genre))
+        assert collector.get_books_with_specific_genre(genre)
 
     def test_get_books_for_children(self, collector):
         assert collector.get_books_for_children()
